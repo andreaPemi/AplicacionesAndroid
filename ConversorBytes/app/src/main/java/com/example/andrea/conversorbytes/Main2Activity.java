13d1e368c19898fc1b468f78737a342c549d2b10
andreaPemi <andrea@MacBook-Pro.local>
2016-02-23 11:20:02 +0100

    añadir repositorios Conversores

---
+++ ConversorBytes/app/src/main/java/com/example/andrea/conversorbytes/Main2Activity.java
@@ -0,0 +1,41 @@
+package com.example.andrea.conversorbytes;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        float dato = 0;
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            dato = extras.getFloat("DATO");
+        }
+
+
+        TextView tv = (TextView) findViewById(R.id.txv2Pantalla);
+        tv.setText("La cantidad convertida es: " + dato);
+    }
+
+}
